098961568508c3a9820b7580192777691b9538e8
yaya <yaya@edu.com>
2024-05-16 21:12:41 +0800

    first commit

---
+++ tlias-pojo/src/main/java/com/itheima/pojo/Dept.java
@@ -0,0 +1,20 @@
+package com.itheima.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * 部门实体类
+ */
+@Data //提供get，set方法
+@NoArgsConstructor //提供无参构造
+@AllArgsConstructor //提供有参构造
+public class Dept {
+    private Integer id; //ID
+    private String name; //部门名称
+    private LocalDateTime createTime; //创建时间
+    private LocalDateTime updateTime; //修改时间
+}
---
+++ tlias-pojo/src/main/java/com/itheima/pojo/PageBean.java
@@ -0,0 +1,16 @@
+package com.itheima.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/*分页查询结果封装类*/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageBean {
+    private long total; //总记录数
+    private List rows; //数据列表
+}
